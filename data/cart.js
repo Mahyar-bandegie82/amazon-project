4bfd4a9c2d757cccfcfb87b260f62d0414b1d337
Mahyar-bandegie82 <mahyarband1382@gmail.com>
2025-11-13 15:59:20 -0800

    fixed cart local storage

--- data/cart.js
+++ data/cart.js
@@ -1,28 +1,15 @@
-export const cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
-    }
-]
+export let cart = JSON.parse(localStorage.getItem('cart'))
 
-// function putCartOut(){
-//     return JSON.parse(localStorage.getItem('cart'))
-// }
+if (!JSON.parse(localStorage.getItem('cart'))) {
+    cart = []
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
-// function putCartIn(){
-//     localStorage.setItem('cart', JSON.stringify(cart))
-// }
-
-// if(!localStorage.getItem('cart')){
-//     localStorage.setItem('cart', JSON.stringify(cart))
-// }
+function putCartIn() {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addProductToCart(productId) {
-    
     const matchingProduct = cart.find((elem) => {
         return elem.productId === productId
     })
@@ -38,20 +25,23 @@ export function addProductToCart(productId) {
         })
     }
 
+    putCartIn();
 }
 
 export function removeProduct(productId) {
-    document.querySelectorAll('.js-delete').forEach((btn)=>{
-        document.querySelectorAll(".js-cart-item-container").forEach((elem)=>{
-            if(productId === elem.dataset.delete){
-                cart.forEach((item, index)=>{
-                    if(item.productId === productId){
+    document.querySelectorAll('.js-delete').forEach((btn) => {
+        document.querySelectorAll(".js-cart-item-container").forEach((elem) => {
+            if (productId === elem.dataset.delete) {
+                cart.forEach((item, index) => {
+                    if (item.productId === productId) {
                         cart.splice(index, 1)
+                        
                         elem.remove()
                         console.log(cart);
                     }
-                }) 
+                })
             }
         })
     })
+    putCartIn()
 }
